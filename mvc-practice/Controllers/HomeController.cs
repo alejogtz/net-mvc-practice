9d3c9a404509b4b87e7f61a7e5f61d30dccca135
Alejo Gutierrez <alejox.gutierrez@gmail.com>
2021-08-20 00:23:17 -0500

    Ejercicios con views y Controller Results

--- mvc-practice/Controllers/HomeController.cs
+++ mvc-practice/Controllers/HomeController.cs
@@ -9,9 +9,42 @@ namespace mvc_practice.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        // ActionName = Index
         public ActionResult Index()
         {
+            // Inferred
+            // View that matches with `Action` name ~/Views/{Controller}/{ActionName}
             return View();
+        }
+
+
+        public ActionResult About()
+        {
+            // Overwrites default view location name by custom view location name;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            // Overwrites default view location name by custom view location name;
+            return View("~/Views/Home/FakeContact.cshtml");
+        }
+
+        //public ActionResult Description()
+        //{
+        //    // Overwrites default view location name by custom view location name;
+        //    return Json(new { mensaje = $"{Request.QueryString}" });
+        //}
+
+        [HttpGet]
+        public JsonResult Description(string name)
+        {
+            // Overwrites default view location name by custom view location name;
+            return Json(new
+            {
+                Nombre = $"¡Hola {name}!",
+                mensaje = $"{Request.QueryString}"
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
